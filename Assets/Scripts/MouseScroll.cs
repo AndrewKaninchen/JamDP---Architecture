1fb7a3029169ae06deb51d50fc7af44aa9a3b136
cordeiro <lucascmgsds@gmail.com>
2018-03-04 17:27:22 -0300

    Agora a camera ta show de bola

--- Assets/Scripts/MouseScroll.cs
+++ Assets/Scripts/MouseScroll.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class MouseScroll : MonoBehaviour {
 
-    public int mdelta = 50;
-    public float mspeed = 200.0f;
+    public int mdelta;
+    public float mspeed;
     private Vector3 direction = Vector3.up;
 	// Use this for initialization
 	void Start () {
-		
+        mdelta = 40;
+        mspeed = 15;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 ruaScreen = Camera.main.WorldToScreenPoint(GameObject.Find("Rua").transform.position);
+        
+
         if(Input.mousePosition.y > Screen.height - mdelta)
         {
             transform.position += direction * Time.deltaTime * mspeed;
         }
-        if(Input.mousePosition.y < mdelta && Input.mousePosition.y > 0)
+        if(Input.mousePosition.y < mdelta && ruaScreen.y < 0)
         {
             transform.position -= direction * Time.deltaTime * mspeed;
         }
