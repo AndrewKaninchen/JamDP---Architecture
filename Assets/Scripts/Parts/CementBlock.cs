32f9102e7d80d5281df095ad6c5ec976d0e5ad54
KonViction <andre.luiz.coelho@outlook.com>
2018-03-04 12:26:40 -0300

    Cement Block fucking works

---
+++ Assets/Scripts/Parts/CementBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CementBlock : Part
+{
+    List<Rigidbody2D> connected = new List<Rigidbody2D>();
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision);
+        var otherRb = collision.rigidbody;
+        var otherCementBlock = otherRb.GetComponent<CementBlock>();
+        if (otherCementBlock != null)
+        {
+            if (otherCementBlock.connected.Contains(GetComponent<Rigidbody2D>()))
+                return;
+        }
+
+        if (!connected.Contains(otherRb))
+        {
+            connected.Add(otherRb);
+            var constraint = gameObject.AddComponent<FixedJoint2D>();
+            constraint.connectedBody = otherRb;
+        }
+    }
+}
