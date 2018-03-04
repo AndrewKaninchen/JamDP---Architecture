10bf1a47baffd4d38cbfa1638c1596e9f617fe81
cordeiro <lucascmgsds@gmail.com>
2018-03-04 17:12:29 -0300

    script para scrollar a camera

---
+++ Assets/Scripts/MouseScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScroll : MonoBehaviour {
+
+    public int mdelta = 50;
+    public float mspeed = 200.0f;
+    private Vector3 direction = Vector3.up;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.mousePosition.y > Screen.height - mdelta)
+        {
+            transform.position += direction * Time.deltaTime * mspeed;
+        }
+        if(Input.mousePosition.y < mdelta && Input.mousePosition.y > 0)
+        {
+            transform.position -= direction * Time.deltaTime * mspeed;
+        }
+    }
+}
